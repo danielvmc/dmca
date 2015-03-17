711142d97c4227bd9e4a1b516f19cfdb1b6697c3
danielvmc <hmtu.tl@gmail.com>
2015-03-17 15:27:16 +0700

    finished episode 6

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,11 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.less('app.less');
+    mix.sass('app.scss');
+
+    mix.styles([
+        'vendor/bootstrap.css',
+        'app.css'
+    ], null, 'public/css');
 });
+
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>DMCA App</title>
 
-	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+	<link href="{{ asset('/css/all.css') }}" rel="stylesheet">
 </head>
 <body>
 	@include('partials.nav')
